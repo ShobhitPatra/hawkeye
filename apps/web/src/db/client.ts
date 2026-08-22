4aa31441b4dc3048c9cba6c7783daa85d095d61d
Shobhit Patra <shobhit9999999@gmail.com>
2026-08-22 19:18:24 +0530

    chore(web): make createDb take a connection string and check the env at the edge

--- apps/web/src/db/client.ts
+++ apps/web/src/db/client.ts
@@ -2,8 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema.js";
 
-export function createDb(connectionString: string | undefined) {
-  if (!connectionString) throw new Error("DATABASE_URL is required");
+export function createDb(connectionString: string) {
   return drizzle(new Pool({ connectionString }), { schema });
 }
 export type Db = ReturnType<typeof createDb>;
